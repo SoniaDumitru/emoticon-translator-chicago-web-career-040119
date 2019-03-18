4eddcb02354fe42964fb8487c66556d8985ed212
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-03-18 18:52:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,10 @@
 # require modules here
 require "yaml"
-path = YAML.load_file('lib/emoticons.yml')
+# emoticons = YAML.load_file('lib/emoticons.yml')
 
 def load_library(path)
   # code goes here
+  emoticons = YAML.load_file(path)
   new_hash = {}
 
 
